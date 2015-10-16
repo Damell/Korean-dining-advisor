06ae498e5991425967743f373c710b31c059dbd7
Daniel Chabr <danielchabr@gmail.com>
2015-10-16 15:52:58 +0900

    Project init setup; taken from Spring Boot Initializr

---
+++ src/main/java/kr/ac/ajou/dsd/kda/KoreanDiningAdvisorApplication.java
@@ -0,0 +1,12 @@
+package kr.ac.ajou.dsd.kda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanDiningAdvisorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreanDiningAdvisorApplication.class, args);
+    }
+}
