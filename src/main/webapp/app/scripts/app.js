912cc3cde7b017df9cd0f5972e124d49d7e5be60
Daniel Chabr <danielchabr@gmail.com>
2015-11-02 21:06:20 +0900

    Generated yeoman Angular project for web app

---
+++ src/main/webapp/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name koreanDiningAdvisorApp
+ * @description
+ * # koreanDiningAdvisorApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('koreanDiningAdvisorApp', [
+    'ngResource'
+  ]);
---
+++ src/main/webapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name koreanDiningAdvisorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the koreanDiningAdvisorApp
+ */
+angular.module('koreanDiningAdvisorApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
