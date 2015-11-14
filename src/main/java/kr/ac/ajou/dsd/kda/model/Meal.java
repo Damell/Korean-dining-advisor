f2c57fdbaa5ee3d4fa3121e8a4177b847d124909
Daniel Chabr <danielchabr@gmail.com>
2015-11-14 17:03:52 +0900

    Photo url attribute added to Meal model

--- src/main/java/kr/ac/ajou/dsd/kda/model/Meal.java
+++ src/main/java/kr/ac/ajou/dsd/kda/model/Meal.java
@@ -28,6 +28,7 @@ public class Meal  {
 	private String description = "";
 	private String[] ingredients = new String[]{""};
 	private String[] category = new String[]{""};
+	private String photoUrl = "";
 	@Embedded
 	private Rating rating = new Rating();
 	private int viewNum = 0;
@@ -138,6 +139,7 @@ public class Meal  {
 		int result = 1;
 		result = prime * result + Arrays.hashCode(category);
 		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((photoUrl == null) ? 0 : photoUrl.hashCode());
 		result = prime * result + ((englishName == null) ? 0 : englishName.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + Arrays.hashCode(ingredients);
@@ -164,6 +166,11 @@ public class Meal  {
 			if (other.description != null)
 				return false;
 		} else if (!description.equals(other.description))
+			return false;
+		if (photoUrl == null) {
+			if (other.photoUrl != null)
+				return false;
+		} else if (!photoUrl.equals(other.photoUrl))
 			return false;
 		if (englishName == null) {
 			if (other.englishName != null)
@@ -197,6 +204,14 @@ public class Meal  {
 		if (viewNum != other.viewNum)
 			return false;
 		return true;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
 	}
 
 }
